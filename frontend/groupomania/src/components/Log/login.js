1c9c2e10942b44a94e44154ff3c9c31fcb6519f8
NASRINE-H <64153322+NASRINE-H@users.noreply.github.com>
2022-06-30 15:07:10 +0200

    login

--- frontend/groupomania/src/components/Log/login.js
+++ frontend/groupomania/src/components/Log/login.js
@@ -4,7 +4,23 @@ const Login = () => {
     return ( 
         <div>
         <h>Login</h>
-        LOGIIIIIIIIIIIIIING
+        <app-login>
+            <form class="ng-untouched ng-pristine ng-invalid">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control ng-untouched ng-pristine ng-invalid" formcontrolname="email" id="email" type="email"></input>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input class="form-control ng-untouched ng-pristine ng-invalid" formcontrolname="password" id="password" type="password"></input>
+                </div>
+                <button color="primary" mat-raised-button="" class="mat-raised-button mat-button-base mat-primary" disabled="true">
+                    <span class="mat-button-wrapper">LOGIN</span>
+                    <div class="mat-button-ripple mat-ripple" matripple=""></div>
+                    <div class="mat-button-focus-overlay"></div>
+                </button>
+            </form>
+        </app-login>
         </div>
     );
 };
